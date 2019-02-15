3d1eba16d16f8dc48b453d2056e2f420ccb0c781
Julien B <julien@toshokan.fr>
2019-02-15 13:15:27 +0100

    Adding forgotten CloudWatch log groups

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -9,3 +9,13 @@ resource "aws_cloudwatch_log_group" "lg_encrypt_snapshot" {
   name              = "/aws/lambda/${aws_lambda_function.encrypt_snapshot.function_name}"
   retention_in_days = "${var.log_retention}"
 }
+
+resource "aws_cloudwatch_log_group" "lg_create_volume" {
+  name              = "/aws/lambda/${aws_lambda_function.create_volume.function_name}"
+  retention_in_days = "${var.log_retention}"
+}
+
+resource "aws_cloudwatch_log_group" "lg_swap_volumes" {
+  name              = "/aws/lambda/${aws_lambda_function.swap_volumes.function_name}"
+  retention_in_days = "${var.log_retention}"
+}
