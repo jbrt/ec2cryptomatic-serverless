1d183ac07997775142d262660132d99e03d16645
Julien B <julien@toshokan.fr>
2019-02-17 11:12:11 +0100

    Adding forgotten CloudWatch log groups

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -19,3 +19,18 @@ resource "aws_cloudwatch_log_group" "lg_swap_volumes" {
   name              = "/aws/lambda/${aws_lambda_function.swap_volumes.function_name}"
   retention_in_days = "${var.log_retention}"
 }
+
+resource "aws_cloudwatch_log_group" "lg_cleanup" {
+  name              = "/aws/lambda/${aws_lambda_function.cleanup_snapshot.function_name}"
+  retention_in_days = "${var.log_retention}"
+}
+
+resource "aws_cloudwatch_log_group" "lg_check_instance" {
+  name              = "/aws/lambda/${aws_lambda_function.check_instance.function_name}"
+  retention_in_days = "${var.log_retention}"
+}
+
+resource "aws_cloudwatch_log_group" "lg_extract_volumes" {
+  name              = "/aws/lambda/${aws_lambda_function.extract_volumes.function_name}"
+  retention_in_days = "${var.log_retention}"
+}
